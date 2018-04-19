Fixing lamin distance calculation.

pygpseq/anim/nucleus.py
@@ -354,7 +354,14 @@ class Nucleus(iot.IOinterface):
 			sig = sig[mid, :, :]
 
 		# Perform distance transform
-		laminD = distance_transform_edt(mask, aspect[3-len(mask.shape):])
+		if 3 == mask.shape:
+			zero_slice = np.zeros((1, mask.shape[1], mask.shape[2]))
+			laminD = distance_transform_edt(
+				np.stack(zero_slice, mask, zero_slice),
+				aspect[3-len(mask.shape):])[1:-1, :, :]
+		else:
+			laminD = distance_transform_edt(mask,
+				aspect[3-len(mask.shape):])
 		centrD = distance_transform_edt(laminD != laminD.max(),
 			aspect[3-len(mask.shape):])
 